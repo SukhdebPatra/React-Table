bc41b710dca9d9631ad78040720c7fe971c1300f
SukhdebPatra <sukhdebpatra1@gmail.com>
2022-06-05 14:11:17 +0530

    Table Sorting

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import BasicTable from './Components/BasicTable';
 import FootorTable from './Components/FooterTable/FootorTable';
 import HeaderGroup from './Components/Header Groups/HeaderGroup';
+import TableSorting from './Components/SortingTable/TableSorting';
 
 function App() {
   return (
 <>
 {/* <BasicTable/> */}
 {/* <FootorTable/> */}
-<HeaderGroup/>
+{/* <HeaderGroup/> */}
+<TableSorting/>
 </>
   );
 }
